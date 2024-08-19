ae26f0b0f84a0333bfcf4f034abd4ab3005b238b
linx <sulinke1133@gmail.com>
2024-08-19 20:16:35 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,4 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-createApp(App).use(ElementPlus, { size: 'middle', zIndex: 3000 }).mount('#app')
+createApp(App).use(ElementPlus, { size: 'default', zIndex: 3000 }).mount('#app')
